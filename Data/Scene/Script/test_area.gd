a3beaa70ac365f2e6e6ce875f660dd5f4e688f78
Jhon Paul Espiritu <jhonpaul.espiritu13@gmail.com>
2024-07-29 04:50:30 -0500

    Created basic wheat sprites. Ensured that tile-setting only works for specific tiles, and that seeds can grow based on updated item dictionary.

--- Data/Scene/Script/test_area.gd
+++ Data/Scene/Script/test_area.gd
@@ -11,9 +11,6 @@ var scene_tile_map : TileMap
 
 # The TileSet ID that is used when selecting a TileMap.
 var tile_set_id : int = 0
-# Desired tile to place when setting a TileMap cell.
-# (Uses Atlas Coords, which are basically coordinates on the TileSet).
-var tile_set_atlas_coord : Vector2 = Vector2(5, 3)
 
 # New variable to hold the player instance
 var player
@@ -38,10 +35,51 @@ func _input(event):
 	
 	# Checks the action when a left click has occured.
 	if Input.is_action_just_pressed("on_left_click"):
+		# Update record variable
 		update_positions()
-		scene_tile_map.set_cell(Global.layer_floor, record_tile_pos, tile_set_id, tile_set_atlas_coord)
+		# Seeds the tile after positions have been updated.
+		seed_tile()
 		
 '''When called, update the positions of the record variables (mouse position, tilemap position, etc.).'''
 func update_positions():
 	record_mouse_pos = get_global_mouse_position()
 	record_tile_pos = scene_tile_map.local_to_map(record_mouse_pos)
+	
+'''Seeds a tile based on the current mouse records.'''
+func seed_tile():
+	# Gets the tile data from mouse position (for checking)
+	var tile_data : TileData = scene_tile_map.get_cell_tile_data(Global.layer_plot, record_tile_pos)
+	
+	# Ensures that tile_data retrieved is valid.
+	if tile_data:
+		# Using the Custom Data Layer boolean, check to make sure tile is seedable.
+		if tile_data.get_custom_data(Global.TILE_SEEDABLE):
+			# Then set the cell to the seed.
+			grow_seed(record_tile_pos, 1, 0)
+			
+'''Grows a seed based on the data from the item dictionary.
+	Tile Position: Used to find the tile to modify.
+	Item ID: Used to index the item data dictionary.
+	Growth Level: Used to identify what stage seed is currently in.
+Currently recurses over tile until it is at the final stage.'''
+func grow_seed(tile_position, item_id, growth_level):
+	# Variables from dictionary:
+	# Actual item data
+	var item_value = Global.dict_seed_data[item_id]
+	# Stage that seed is currently in
+	var item_stage = item_value["Stages"][growth_level]
+	# Atlas coord to replace the current tile.
+	var item_atlas_coord = item_value["Atlas"][growth_level]
+	
+	# Sets the selected tile with the seed's atlas coord.
+	scene_tile_map.set_cell(Global.layer_floorobj, tile_position, tile_set_id, item_atlas_coord)
+	# Ensures that the current stage isn't the final one.
+	if item_stage == item_value["Final"]:
+		return
+	var item_timing = item_value["Timings"][growth_level]
+	
+	# Creates a timer in the tree, and awaits its timeout using the current item timing.
+	await get_tree().create_timer(item_timing).timeout
+	# Recurses, adding a growth level each loop.
+	grow_seed(tile_position, item_id, growth_level + 1)
+	
--- global.gd
+++ global.gd
@@ -3,6 +3,22 @@ extends Node
 
 # Specifies the layer numbers when interacting with the tilemap.
 var layer_floor = 0
-var layer_floorobj = 1
-var layer_wall = 2
-var layer_air = 3
+var layer_plot = 1
+var layer_floorobj = 2
+var layer_wall = 3
+var layer_air = 4
+
+# Nested Dictionary storing seed data based on ID.
+var dict_seed_data = {
+	1 : {
+		"Name" : "Wheat",
+		"Stages" : ["seed", "stub", "grown"],
+		"Atlas" : [Vector2(19, 14), Vector2(20, 14), Vector2(21, 14)],
+		"Timings" : [2.2, 2.1],
+		"Final" : "grown"
+	}
+}
+
+# String when checking for tile validity.
+# Used to check if tile can be seeded.
+const TILE_SEEDABLE = "seedable"
