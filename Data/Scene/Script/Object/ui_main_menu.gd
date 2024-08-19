f15f4561ccbc51f480751b5c1a8cae9f37536e53
HsiaolanKung <165440570+HsiaolanKung@users.noreply.github.com>
2024-08-19 18:53:11 -0400

    adds credits page

--- Data/Scene/Script/Object/ui_main_menu.gd
+++ Data/Scene/Script/Object/ui_main_menu.gd
@@ -1,14 +1,28 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var credits_menu = $Credits_menu
+@onready var play_button = $Play
+@onready var credits_button = $Credits
+@onready var title = $Label
 
+var credits_on: bool = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
+func _on_credits_pressed():
+	if credits_on:
+		credits_menu.hide()	
+		play_button.show()
+		credits_button.show()
+		title.show()
+	else:
+		credits_menu.show()
+		play_button.hide()
+		credits_button.hide()
+		title.hide()
+	credits_on = !credits_on
+	
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Data/Scene/Object/Environment/test_area.tscn")
+	
+func _on_texture_button_pressed():
+	_on_credits_pressed()
