fb0cf685bb4a6cfeeeab5dab3fe7549c31864650
HsiaolanKung <165440570+HsiaolanKung@users.noreply.github.com>
2024-08-29 00:41:13 -0400

    Implements other crops within the game.

--- Data/Scene/Script/coin_count.gd
+++ Data/Scene/Script/coin_count.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var score_label = $Coinbutton/Label2
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	score_label.text = str(Global.score)
-
